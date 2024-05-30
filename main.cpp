de93716181644502412393f6912060ff41b9d2d4
GaseousStarfish <Gaseous_starfish@163.com>
2024-05-30 22:43:26 +0800

    Base Point
    Base Point was created

---
+++ main.cpp
@@ -0,0 +1,96 @@
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+#include<iostream>
+#include<string>
+#include<fstream>
+#define numVAOs 1
+using namespace std;
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+
+string readShaderSource(const char* filePath)
+{
+	string content;
+	ifstream fileStream(filePath, ios::in);
+	string line = "";
+	while (!fileStream.eof())
+	{
+
+		getline(fileStream, line);
+		content.append(line + "\n");
+	}
+	fileStream.close();
+	
+	return content;
+}
+
+GLuint createShaderProgram()
+{
+	string vertShaderStr = readShaderSource("vertShader.glsl");
+
+	string fragShaderStr = readShaderSource("fragShader.glsl");
+
+	const char* vertShaderSrc = vertShaderStr.c_str();
+	const char* fragShaderSrc = fragShaderStr.c_str();
+
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+	
+	glShaderSource(vShader, 1, &vertShaderSrc, NULL);
+	glShaderSource(fShader, 1, &fragShaderSrc, NULL);
+
+	glCompileShader(vShader);
+	glCompileShader(fShader);
+
+	GLuint vfProgram = glCreateProgram();
+	glAttachShader(vfProgram, vShader);
+	glAttachShader(vfProgram, fShader);
+	glLinkProgram(vfProgram);
+
+	return vfProgram;
+}
+
+
+
+void init(GLFWwindow* window)
+{
+	renderingProgram = createShaderProgram();
+	glGenVertexArrays(numVAOs, vao);
+	glBindVertexArray(vao[0]);
+}
+void display(GLFWwindow* window, double currentTime)
+{
+	//glClearColor(1.0, 0.0, 0.0, 1.0);
+	//glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(renderingProgram);
+	glPointSize(30.0f);
+	glDrawArrays(GL_POINTS, 0, 1);
+}
+
+int main(void)
+{
+	if (!glfwInit()) { exit(EXIT_FAILURE); };
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "test", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+	glfwSwapInterval(1);
+
+	init(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+
+}
