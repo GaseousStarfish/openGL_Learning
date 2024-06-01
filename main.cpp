ba2e0ab2cbc2826bd6417ec8cae787a8ab0b0b0d
GaseousStarfish <Gaseous_starfish@163.com>
2024-06-01 16:08:18 +0800

    triangle

--- main.cpp
+++ main.cpp
@@ -66,7 +66,8 @@ void display(GLFWwindow* window, double currentTime)
 
 	glUseProgram(renderingProgram);
 	glPointSize(30.0f);
-	glDrawArrays(GL_POINTS, 0, 1);
+	//glDrawArrays(GL_POINTS, 0, 1);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
 int main(void)
